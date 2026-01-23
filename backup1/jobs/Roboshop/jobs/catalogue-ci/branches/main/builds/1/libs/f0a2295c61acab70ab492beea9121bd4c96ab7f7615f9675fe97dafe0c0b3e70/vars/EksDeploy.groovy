250a6d6fff54a9c4b6c03e74f7888d64e48d9fdc
root <root@ip-172-31-78-0.ec2.internal>
2026-01-23 07:48:35 +0000

    added backup

---
+++ backup1/jobs/Roboshop/jobs/catalogue-ci/branches/main/builds/1/libs/f0a2295c61acab70ab492beea9121bd4c96ab7f7615f9675fe97dafe0c0b3e70/vars/EksDeploy.groovy
@@ -0,0 +1,97 @@
+def call(Map config){
+
+    pipeline{
+        agent any
+
+        environment {
+          COURSE    = "Jenkins"                      // Training / reference variable (not used in pipeline logic)
+          appVersion = config.get("appVersion")
+          ACC_ID    = "471112667143"               // AWS Account ID
+          PROJECT   = config.get("project")       // Project name (used in EKS cluster naming)
+          COMPONENT = config.get("component")    // Application / microservice name
+          REGION    = "us-east-1"               // AWS region where EKS clusters exist
+          DEPLOY_TO =  config.get("deployTo")
+        }
+
+        options {
+          timeout(time: 30, unit: 'MINUTES')    // Abort build if it runs longer than 30 minutes
+          disableConcurrentBuilds()            // Prevent multiple builds of this job at the same time
+        }
+
+        /* parameters {
+
+          // Application version to deploy (example: 1.0.3, latest, commit-id)
+          string(
+            name: 'appVersion',
+            description: 'Which app version you want to deploy'
+          )
+
+
+          // * This selects the TARGET ENVIRONMENT / EKS CLUSTER.
+          // *
+          // * dev  -> roboshop-dev  EKS cluster
+          // * qa   -> roboshop-qa   EKS cluster
+          // * prod -> roboshop-prod EKS cluster
+          // *
+          // * NOTE:
+          // * This parameter DOES NOT select a Git branch.
+          // * It is only used to decide which Kubernetes cluster Jenkins connects to.
+
+          choice(
+            name: 'deploy_to',
+            choices: ['dev', 'qa', 'prod'],
+            description: 'Select which EKS cluster to connect to'
+          )
+        }
+         */
+
+        stages{
+          stage('Deploy'){
+                          steps{
+                            script{
+                              withAWS(region: 'us-east-1', credentials: 'aws-creds') {
+                                    sh """
+                                      set -e
+                                      aws eks update-kubeconfig --region ${REGION} --name ${PROJECT}-${DEPLOY_TO}
+                                      kubectl get nodes
+                                      sed -i "s/IMAGE_VERSION/${appVersion}/g" values.yaml
+                                      helm upgrade --install ${COMPONENT} -f values-${DEPLOY_TO} -n ${PROJECT} --atomic --timeout=5m .
+                                    """
+                              }
+                            }
+                          }
+                      }
+
+          stage('Functiona Testing'){
+            when{
+              expression { DEPLOY_TO == "dev" }
+            }
+                  steps{
+                    script{
+                      sh """
+                            echo "functional Testing "
+                      """
+                    }
+                  }
+          }
+        }
+
+         post{
+              always{
+                      echo 'I will always say Hello again!'
+                      cleanWs()
+                  }
+                  success {
+                      echo 'I will run if success'
+                  }
+                  failure {
+                      echo 'I will run if failure'
+                  }
+                  aborted {
+                      echo 'pipeline is aborted'
+                  }
+             }
+
+    }
+
+}
